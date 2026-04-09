68105c633eb0a1b311dc8d7bd6e1d71cf44d8e55
Wojciech Kozieja <kozieja.wojciech@gmail.com>
2026-04-09 11:38:07 +0200

    adding_logs

--- api/scheduled_deactivate_offers.py
+++ api/scheduled_deactivate_offers.py
@@ -1,18 +1,25 @@
 from sqlalchemy import func
 from database import SessionLocal
 from models import Offer
+import logging
 
+logger = logging.getLogger(__name__)
 
 def deactivate_offers():
     db = SessionLocal()
     try:
-        db.query(Offer).filter(
+        count = db.query(Offer).filter(
             Offer.is_active == True,
             Offer.end_offer_date < func.now()
         ).update({"is_active": False})
         db.commit()
+        logger.info(f"Deactivated {count} offers")
+    except Exception as e:
+        logger.error(f"ERROR during deactivating offers: {e}")
+        db.rollback()
     finally:
         db.close()
+
 
 
 if __name__ == "__main__":
